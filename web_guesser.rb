7b2e2581ca19267b3d3994e89830538a6731861a
Kyle <kylelinwu@gmail.com>
2017-07-28 18:05:30 -0700

    Colorized the background based on validity of guess

--- web_guesser.rb
+++ web_guesser.rb
@@ -9,8 +9,24 @@ def check_guess(guess)
   return "Too low!" if guess < SECRET_NUMBER
   "You got it right! The SECRET NUBMER is #{SECRET_NUMBER}."
 end
+
+def color_check(guess)
+  guess = guess.to_i
+  if SECRET_NUMBER - guess >= 5 || guess - SECRET_NUMBER >= 5
+    :red
+  elsif SECRET_NUMBER == guess
+    :springgreen
+  else
+    :lightpink
+  end
+end
 get '/' do
   guess = params['guess']
   message = check_guess(guess)
-  erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
+  color = color_check(guess)
+  erb :index, :locals => {
+    :number => SECRET_NUMBER,
+    :message => message,
+    :background_color => color
+  }
 end
