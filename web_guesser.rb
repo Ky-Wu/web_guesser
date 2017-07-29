ab6a6f79df27ff770c680f9caaf45b73e9e5964e
Kyle <kylelinwu@gmail.com>
2017-07-28 21:34:19 -0700

    Added guess limiting

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,32 +1,48 @@
 require "sinatra"
 require 'sinatra/reloader'
-SECRET_NUMBER = rand(100)
-def check_guess(guess)
+@@guesses = 5
+$secret_number = rand(100)
+def guess_message(guess)
   guess = guess.to_i
-  return "Way too high!" if guess - SECRET_NUMBER >= 5
-  return "Too high!" if guess > SECRET_NUMBER
-  return "Way too low!" if SECRET_NUMBER - guess >= 5
-  return "Too low!" if guess < SECRET_NUMBER
-  "You got it right! The SECRET NUBMER is #{SECRET_NUMBER}."
+  if guess == $secret_number
+    return "You got it right! The SECRET NUBMER was #{$secret_number}. "\
+           "Guess the new number!"
+  end
+  @@guesses -= 1
+  if @@guesses == 0
+    return "You lost! You're out of guesses. Guess the new number."
+  end
+  return "Way too high!" if guess - $secret_number >= 5
+  return "Too high!" if guess > $secret_number
+  return "Way too low!" if $secret_number - guess >= 5
+  return "Too low!" if guess < $secret_number
 end
 
 def color_check(guess)
   guess = guess.to_i
-  if SECRET_NUMBER - guess >= 5 || guess - SECRET_NUMBER >= 5
+  if $secret_number - guess >= 5 || guess - $secret_number >= 5
     :red
-  elsif SECRET_NUMBER == guess
+  elsif $secret_number == guess
     :springgreen
   else
     :lightpink
   end
 end
+
+def generate_new_number
+  $secret_number = rand(100)
+  @@guesses = 5
+end
+
 get '/' do
   guess = params['guess']
-  message = check_guess(guess)
+  message = guess_message(guess)
   color = color_check(guess)
+  generate_new_number if @@guesses == 0 || guess == $secret_number
   erb :index, :locals => {
-    :number => SECRET_NUMBER,
+    :number => $secret_number,
     :message => message,
-    :background_color => color
+    :background_color => color,
+    :guess_count => @@guesses.to_s
   }
 end
